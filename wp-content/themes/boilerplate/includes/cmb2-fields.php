82884dac7c6ff922f10304b8e2d9198aa095e105
Lisa Thomas <lisathomas@Lisas-MacBook-Pro-2.local>
2018-06-18 21:42:19 -0700

    add files

---
+++ wp-content/themes/boilerplate/includes/cmb2-fields.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Fifty Boilerplate functions and definitions
+ *
+ * @package WordPress
+ * @subpackage Fifty Boilerplate
+ * @since Fifty Boilerplate 1.0
+ * @link https://github.com/WebDevStudios/CMB2
+ */
+
+// Hero Metafields
+// require_once get_stylesheet_directory() . '/includes/fields/cmb2-hero.php';
+
+// UI Kit fields
+// require_once get_stylesheet_directory() . '/includes/fields/cmb2-uikit.php';
